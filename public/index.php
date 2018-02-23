efcb21bf7ee2a6b7502c5d39ea195c8912a902bd
brice <brice@libcast.com>
2018-02-23 18:26:41 +0100

    Add support for requests with Json body

--- public/index.php
+++ public/index.php
@@ -9,6 +9,7 @@ use JobQueue\Application\Http\ApiError;
 use JobQueue\Application\Http\ApiRouting;
 use Middlewares\ErrorHandler;
 use Middlewares\FastRoute;
+use Middlewares\JsonPayload;
 use Middlewares\RequestHandler;
 use Relay\Relay;
 use Zend\Diactoros\ServerRequestFactory;
@@ -16,6 +17,8 @@ use Zend\Diactoros\ServerRequestFactory;
 $response = (new Relay([
 
     (new ErrorHandler(new ApiError))->catchExceptions(true),
+
+    new JsonPayload,
 
     new FastRoute(ApiRouting::createFromCache("$root/cache/routing.php")),
 
