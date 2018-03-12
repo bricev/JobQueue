bac2039365d1c90cd3a1ad49ceb8e530f7622127
brice <brice@libcast.com>
2018-03-12 10:56:01 +0100

    Set logger from constructor

--- src/Application/Console/WorkerApplication.php
+++ src/Application/Console/WorkerApplication.php
@@ -3,27 +3,25 @@
 namespace JobQueue\Application\Console;
 
 use JobQueue\Domain\Task\Queue;
-use Psr\Log\LoggerAwareInterface;
-use Psr\Log\LoggerAwareTrait;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Application;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
-final class WorkerApplication extends Application implements LoggerAwareInterface
+final class WorkerApplication extends Application
 {
-    use LoggerAwareTrait;
-
     /**
      *
      * @param Queue                    $queue
      * @param EventDispatcherInterface $eventDispatcher
+     * @param LoggerInterface|null     $logger
      */
-    public function __construct(Queue $queue, EventDispatcherInterface $eventDispatcher)
+    public function __construct(Queue $queue, EventDispatcherInterface $eventDispatcher, LoggerInterface $logger = null)
     {
         parent::__construct('worker', 1.0);
 
         $consumeCommand = new Consume($queue, $eventDispatcher);
-        if ($this->logger) {
-            $consumeCommand->setLogger($this->logger);
+        if ($logger) {
+            $consumeCommand->setLogger($logger);
         }
 
         $this->add($consumeCommand);
